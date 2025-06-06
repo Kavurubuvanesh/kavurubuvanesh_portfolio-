7cfd95e68d551aa6267c5e9d0ad528e7aa094472
Kavuru Buvanesh <169604812+Kavurubuvanesh@users.noreply.github.com>
2025-06-06 20:13:31 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/smoke-298243_1280.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "black and smoky grey abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Al/ML student studying at KIIT University. I enjoy exploring new technologies, and contributing to open source.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "AI Enginner",
+  "Generative AI",
+  "Coding and Programming",
+  "Machine Learning",
+  "AutoCAD",
+  "Azure",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I love exploring new technologies, building projects. By leveraging my AI/ML experience I continually look for new and better ways to make tech accessible by all. I believe in the power of collaboration and open source to drive innovation and create impactful solutions.";
 
 const About = () => {
   return (
